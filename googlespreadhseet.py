0a4dc2fafcbc5e4971935043d0bc9700d3c54e1b
kennethpino <ken.pino@gmail.com>
2023-08-26 05:58:07 +0200

    changes to update spread sheet complete

--- googlespreadhseet.py
+++ googlespreadhseet.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 import requests
@@ -7,38 +6,28 @@ from seachengine import SearchEngine
 
 SPREAD_SHEET_ID = os.environ['SPREAD_SHEET_ID']
 SPREAD_SHEET_ENDPOINT = f'https://api.sheety.co/{SPREAD_SHEET_ID}/flightDeals/prices'
-CITY_WISH_LIST = 'flight_wish_list.json'
-
-
-def save_data(response):
-    with open(CITY_WISH_LIST, 'w') as file:
-        file.write(json.dumps(response.json(), indent=4))
-
-
-def get_city_list():
-    """Returns a list of cities previously saved from the online spreadsheet"""
-    try:
-        with open(CITY_WISH_LIST) as file:
-            flight_wish_list_dict = json.load(file)
-    except FileNotFoundError:
-        print(f'Unable to locate {CITY_WISH_LIST}')
-    else:
-        return [item['city'] for item in flight_wish_list_dict['prices']]
+SHEETY_TOKEN = os.environ['SHEETY_TOKEN']
 
 
 class GoogleSpreadSheet:
     def __init__(self):
         self.endpoint = SPREAD_SHEET_ENDPOINT
         self.search_engine = SearchEngine()
+        self.sheety_token = SHEETY_TOKEN
 
     def update_iata_codes(self):
         """Updates every record on the spreadsheet with the corresponding iata codes for each city"""
-        # Get records from the spreadsheet
-        # UNCOMMENT THESE LINES TO PERFORM A REQUEST
-        # response = requests.get(self.endpoint)
-        # response.raise_for_status()
-        # save_data(response)
 
-        city_list = get_city_list()
+        headers = {
+            'Authorization': self.sheety_token
+        }
 
-        self.search_engine.get_iata_code(city_list[0])
+        for item in requests.get(self.endpoint).json()['prices']:  # Get list of items in the spreadsheet
+            params = {
+                'price': {
+                    'city': item['city'].title(),
+                    'iataCode': self.search_engine.get_iata_code(item['city']),
+                    'lowestPrice': item['lowestPrice']
+                }
+            }
+            requests.put(f"{self.endpoint}/{item['id']}", json=params, headers=headers)
--- seachengine.py
+++ seachengine.py
@@ -1,17 +1,10 @@
 import os
-import json
 
 import requests
 
 ENV_KIWI_API_KEY = os.environ['ENV_KIWI_API_KEY']
 ENDPOINT_SEARCH = 'https://api.tequila.kiwi.com'
 IATA_CODE_SEARCH_ENDPOINT = '/locations/query'
-CITY_SEARCH_RESULTS = 'city_search_results.json'
-
-
-def save_data(response):
-    with open(CITY_SEARCH_RESULTS, 'w') as file:
-        file.write(json.dumps(response.json(), indent=4))
 
 
 class SearchEngine:
@@ -23,24 +16,14 @@ class SearchEngine:
     def get_iata_code(self, city):
         params = {
             'term': city,
-            'location_types': 'city'
+            'location_types': 'city',
+            'limit': 1
         }
 
         headers = {
             'apikey': self.apikey
         }
 
-        print(f'{self.endpoint}{self.endpoint_iata_code}')
-
-        # response = requests.get(f'{self.endpoint}{self.endpoint_iata_code}', params=params, headers=headers)
-        # response.raise_for_status()
-        # save_data(response)
-
-        try:
-            with open('city_search_results.json') as file:
-                city_search_results_dict = json.load(file)
-        except FileNotFoundError:
-            print(f'Unable to locate {CITY_SEARCH_RESULTS}')
-        else:
-            print(city_search_results_dict['locations'][0]['code'])
-
+        response = requests.get(f'{self.endpoint}{self.endpoint_iata_code}', params=params, headers=headers)
+        response.raise_for_status()
+        return response.json()['locations'][0]['code']
